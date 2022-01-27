0fd92a2ac9f8faf0d8dda041c0720aa1394a395a
intercept6 <4459220+intercept6@users.noreply.github.com>
2022-01-27 11:29:54 +0900

    chore: lintの設定

--- lib/cdk-strapi-on-ecs-stack.ts
+++ lib/cdk-strapi-on-ecs-stack.ts
@@ -1,16 +1,8 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Stack, StackProps} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
 
 export class CdkStrapiOnEcsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStrapiOnEcsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
